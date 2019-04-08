6775302032be0e0bcaa07b26f5743e58082f6d3e
hala22 <228479495@qq.com>
2019-04-08 10:29:45 +0800

    hw

--- 13 of hw4.java
+++ 13 of hw4.java
@@ -7,19 +7,16 @@ public class HomeWork {
 	public static void main(String[] args) {
 	
 		
-		
-		for(int i=1;i<=1000;i++){
-			int temp=0;
-			// 这里只需要检验到i的一半就够了，提高效率
-			for(int j=1;j<i/2;j++){
-				if(i%j==0){
-					temp+=j;
+		for(int i=2;i<=1000;i++) {
+			int flag=i;
+			for(int j=1;j<=i/2;j++) {
+				if(i%j==0) {
+					flag-=j;
 				}
 			}
 			
-			if(temp==i){
-			System.out.print(i+"  ");
-			}
+			if(flag==0)
+				System.out.println(i);
 		}
 		
 	}
